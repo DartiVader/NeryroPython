adaeb0ceb9aef1f26f8e29ffebef62f43f6acf45
viqtor04 <145802400+viqtor04@users.noreply.github.com>
2023-10-02 05:37:32 +0300

    Modified: main.py Worker.py db_dlya_nero.db

--- source/Worker.py
+++ source/Worker.py
@@ -13,12 +13,15 @@ class Worker:
     def __init__(self):
         self.fname = open("./namelist/fname.txt").read().split()
         self.sname = open("./namelist/sname.txt").read().split()
+        self.con = sqlite3.connect("../db_dlya_nero.db")
+        self.cur = self.con.cursor()
 
     def create_worker(self, count, i=[1, 10], s=[1, 10], c=[1, 10], e=[1, 10]):
-        con = self.connect_base()
         for element in range(count):
             id = random.randint(1, 1000000000)
             name = random.choice(self.fname) + " " + random.choice(self.sname)
+            busy = 0
+            work_description = "unemployed"
             skills = {
                 "i": random.randint(i[0], i[1]),
                 "s": random.randint(s[0], s[1]),
@@ -26,11 +29,17 @@ class Worker:
                 "e": random.randint(e[0], e[1])
             }
 
-            con.cursor().execute("INSERT INTO "
+            self.cur.execute("INSERT INTO "
                                  "users (Id, Name, Strength, Endurance, Intelligence, Charisma) "
                                  "VALUES (?, ?, ?, ?, ?, ?)",
                                  (id, name, skills['s'], skills['e'], skills['i'], skills['c']))
-            con.commit()
+            self.cur.execute("INSERT INTO "
+                             "users_zanyatost (Id, Busy, Work_description) "
+                             "VALUES (?, ?, ?)",
+                             (id, busy, work_description))
+            self.con.commit()
 
-    def connect_base(self):
-        return sqlite3.connect("../db_dlya_nero.db")
+    def delete_worker(self):
+        self.cur.execute("DELETE FROM users WHERE id > 0")
+        self.cur.execute("DELETE FROM users_zanyatost WHERE id > 0")
+        self.con.commit()
