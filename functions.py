ed541efa324fc0bbec436b8627e05e7ea171c373
artem210905 <146650656+artem210905@users.noreply.github.com>
2023-10-10 21:17:34 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,4 +1,20 @@
-def add_new_verb(glag):
+import sqlite3
+
+
+connection = sqlite3.connect('db_glagoli.db')
+cursor = connection.cursor()
+
+
+def add_new_verb(glag, sr_spec):
+    cursor.execute(f"SELECT * FROM glagolnew WHERE Glag ='{glag}'")
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO glagolnew VALUES (?, ?, ?, ?, ?, ?)", (1, glag, sr_spec[0], sr_spec[1], sr_spec[2], sr_spec[3],))
+        connection.commit()
+    else:
+        string = cursor.fetchall()
+        vhodi = string[0] + 1
+        cursor.execute('''UPDATE glagolnew SET vhod = ? WHERE Glag = ?''', (vhodi, glag))
+
     '''
     поиск glag (строка с глаголом) в новой табллице
     если нашли измененяем характеристики и увеличиваем колво вхождений слова в в таблицу на 1
